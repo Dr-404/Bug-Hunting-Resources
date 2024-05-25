ed2ab03cfde9a1e19031162df012bf15e97f939e
Dr.404 <drhtunaungkyaw1993@gmail.com>
2024-05-25 16:24:44 -0400

    finishing subdomain finding

--- tools/Total_Recon/total_recon.sh
+++ tools/Total_Recon/total_recon.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This following tools need to install for this recon tools
+
+
+# sudo apt install golang-go ( Install `go` )
+# go install github.com/tomnomnom/httprobe@latest ( Install `httprobe` )
+
+
+
 # ANSI color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -7,9 +15,22 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Global Variables
-domain="mytel.com.mm"
+domain="springdevelopmentbank.com"
 api_key='DDKfDTHXTY17r1qYBq78FbQjvFa93Adw'
 output_dir="output"
+
+
+subdomain_file="$output_dir/new_subdomains.txt"
+
+# To store Tempory result and delet after fin result
+
+tmp_active_domain="$output_dir/active_sub.txt"
+tmp_inactive_domain="$output_dir/inactive_sub.txt"
+
+# Final Output Dir
+
+active_domain="$output_dir/active_subdomains.txt"
+inactive_domain="$output_dir/inactive_subdomains.txt"
 
 function check_output_folder {
     if [ ! -d "$output_dir" ]; then
@@ -34,7 +55,7 @@ function subdomain_finder {
     echo "$response" | jq -r '.subdomains[]' | while read -r subdomain; do
         echo "$subdomain.$domain" >> "$output_dir/subdomains.txt"
     done
-    echo -e "[${GREEN}+${NC}] Adding subdomains to $PWD/$output_dir/subdomains.txt"
+    
 }
 
 
@@ -48,9 +69,9 @@ function add_http {
 
 function validate_subdomain {
 
-	subdomain_file="$output_dir/new_subdomains.txt"
-    active_subdomains_file="$output_dir/active_subdomains.txt"
-    inactive_subdomains_file="$output_dir/inactive_subdomains.txt"
+	
+
+    
 
     if [ ! -f "$subdomain_file" ]; then
     	echo -e "[${RED}ERROR${NC}] Subdomain file $subdomain_file does not exist. Exiting."
@@ -58,8 +79,8 @@ function validate_subdomain {
 
     fi
 
-    > "$active_subdomains_file"
-    > "$inactive_subdomains_file"
+    > "$tmp_active_domain"
+    > "$tmp_inactive_domain"
 
     
     while IFS= read -r subdomain; do
@@ -68,52 +89,54 @@ function validate_subdomain {
         https_status=$(curl -s -o /dev/null -w "%{http_code}" "$subdomain")
 
         if [[ "$http_status" -eq 200 || "$http_status" -eq 301 || "$http_status" -eq 308||"$https_status" -eq 403 ]]; then
-            echo -e "${GREEN}$subdomain is active (HTTP) with status $http_status${NC}"
-            echo "$subdomain" >> "$active_subdomains_file"
+
+            #echo -e "${GREEN}$subdomain is active (HTTP) with status $http_status${NC}"
+
+            echo "$subdomain" >> "$tmp_active_domain"
+            
         else
-            echo -e "${RED}$subdomain is not active${NC}with status $https_status"
-            echo "$subdomain" >> "$inactive_subdomains_file" 
+            #echo -e "${RED}$subdomain is not active${NC}with status $https_status"
+
+            echo "$subdomain" >> "$tmp_inactive_domain" 
         fi
     done < "$subdomain_file"
-    rm -rf $subdomain_file
 
+    
 
-    # http_status=$(httpx -status-code -l "$subdomain_file" 2>/dev/null)    
-
-    # echo "$http_status\n" | grep -E '200|301' | awk '{print $1 $2}'
-    # echo "$http_status\n" | grep '\b4[0-9][0-9]\b' | awk '{print $1 $2}'
+    
 
 }
 
-# function validate {
-  
+function clean_result {
 
-#     cat $subdomain_file | httprobe >> "$output_dir/new_subdomains.txt"
-#     new_subdomain="$output_dir/new_subdomains.txt"
+    # Remove Unnessary Files
 
-#     while IFS= read -r sub; do
-#     	#httprobe $sub > 
-#     	echo $sub
-#         http_status=$(httpx -status-code -u "$sub")
-        
-#         echo "........................"
-        
-#         if [[ "$http_status" =~ ^(200|301)$ ]]; then
-#             echo "$subdomain" >> "$active_subdomains_file"
-#             echo "Success 0"
-#         else
-#             echo "$subdomain" >> "$inactive_subdomains_file"
-#             echo "Success 2"
-#         fi
-        
-#     done < "$new_subdomain"
+    rm $output_dir/subdomains.txt
+    rm $subdomain_file
 
-#     echo -e "${GREEN}[+] Check and Validate subdomain ...${NC}"
-#     echo -e "${RED}[-] Removing inactive subdomain and export as $inactive_subdomains_file${NC}"
-#     echo -e "${GREEN}[+] Adding valid subdomain to $PWD/$output_dir/active_subdomains_file.txt${NC}"
-# }
+    # Reomve Temporary Active Result
+
+    awk -F'//' '{print $2}' "$tmp_active_domain" | sort | uniq > $active_domain
+    rm -rf $tmp_active_domain
+
+    echo -e "[${GREEN}+${NC}] Adding active subdomains to $active_domain"
+
+    # Remove Tempory Inactive Result
+
+    awk -F'//' '{print $2}' "$tmp_inactive_domain" | sort | uniq > $inactive_domain
+    rm -rf $tmp_inactive_domain
+
+    echo -e "[${RED}-${NC}] Adding inactive subdomains to $inactive_domain"
+    
+
+}
+
+
+# Main Function
+#==============# 
 
 check_output_folder
 subdomain_finder
 add_http
 validate_subdomain
+clean_result
